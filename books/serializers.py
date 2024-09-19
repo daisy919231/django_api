27e2eb494560a4fd3e5fb96815af24a528896dbf
Daisy <shahzodaakhmedova91@gmail.com>
2024-09-19 06:01:05 +0300

    committed

--- books/serializers.py
+++ books/serializers.py
@@ -5,4 +5,4 @@ from .models import Book
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
-        fields = '__all__'  # or specify fields like ['id', 'title', 'author', ...]
+        fields = '__all__'  
