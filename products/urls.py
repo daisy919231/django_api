6065876ca9988675eb6f2477797001f3c7e2bba8
Daisy <shahzodaakhmedova91@gmail.com>
2024-09-24 17:21:53 +0300

    zaxira

--- products/urls.py
+++ products/urls.py
@@ -7,13 +7,16 @@ urlpatterns = [
     path('category_list_simple/', CategoryListBase.as_view(), name='simple_category_list'),
     path('product_create_simple/', ProductCreateAPI.as_view(), name='simple_create'),
     path('product_list_simple/', ProductListAPI.as_view(), name='simple_list'),
-    path('product_update_simple/', ProductUpdateAPI.as_view(), name='simple_update'),
-    path('product_delete_simple/', ProductDestroyAPI.as_view(), name='simple_delete'),
+    path('product_update_simple/<int:pk>/', ProductUpdateAPI.as_view(), name='simple_update'),
+    path('product_delete_simple/<int:pk>/', ProductDestroyAPI.as_view(), name='simple_delete'),
     path('product_list_create/', ProductListCreateAPI.as_view(), name='list_create'),
-    path('product_simple_retrieve', ProductRetrieveAPI.as_view(), name='simple_retrive'),
-    path('product_retrieve_update/', ProductRetrieveUpdateAPI.as_view(), name='retrive_update'),
-    path('product_retrieve_delete/', ProductRetrieveDestroyAPI.as_view(), name='retrieve_delete'),
-    path('product_complex/', ProductRetrieveUpdateDestroyAPI.as_view(), name='product_complex')
+    path('product_simple_retrieve/<int:pk>/', ProductRetrieveAPI.as_view(), name='simple_retrive'),
+    path('product_retrieve_update/<int:pk>/', ProductRetrieveUpdateAPI.as_view(), name='retrive_update'),
+    path('product_retrieve_delete/<int:pk>/', ProductRetrieveDestroyAPI.as_view(), name='retrieve_delete'),
+    path('product_complex/<int:pk>/', ProductRetrieveUpdateDestroyAPI.as_view(), name='product_complex')
     
 
 ]
+
+# IMPORTANT!
+# ALWAYS WRITE A PK IN THE URL, BECAUSE HOW DOES IT KNOW WHICH INSTANCE IT WANTS TO CHANGE?!
