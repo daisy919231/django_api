467a6c8af558079939aa2810a9635a6d489fe4e2
Daisy <shahzodaakhmedova91@gmail.com>
2024-10-03 07:05:09 +0300

    customized

--- books/views.py
+++ books/views.py
@@ -11,12 +11,17 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import authentication, permissions
 from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
 
 
 class BookListCreateAPIView(generics.ListCreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
 
 # class BookDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
 #     queryset = Book.objects.all()
@@ -41,12 +46,18 @@ class BookListCreateAPIView(generics.ListCreateAPIView):
 #         return Response(usernames)
 
 class BookListAPIView(APIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
     def get (self, request):
         books=Book.objects.all()
         serializer=BookSerializer(books, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
 class BookCreateAPIView(APIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
     def get (self, request):
         books=Book.objects.all()
         serializer=BookSerializer(books, many=True)
--- root/urls.py
+++ root/urls.py
@@ -22,13 +22,13 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
-
+from root.simplejwt_tokens import CustomObtainPairView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('books/', include('books.urls')),
     path('api-auth/', include('rest_framework.urls')),
     path('products/', include('products.urls')),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/', CustomObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
 ]
